1783a9628fa6c19a902854ae900cd9742a6ac85e
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2021-09-08 13:10:37 +0100

    finished POV code

--- server/server.js
+++ server/server.js
@@ -4,9 +4,12 @@ const publisher = require("./centrifugoPublisher");
 
 const app = express();
 
-publisher.publish("channel", { jon: "snow" });
-
 app.use(express.static(path.join(process.cwd(), "public")));
+
+app.get("/publish", (req, res) => {
+  publisher.publish("channel_name", { jon: "snow" });
+  res.send("ok");
+});
 
 app.listen(5000, () => {
   console.log(`listening on port 5000`);
